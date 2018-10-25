2ba7c95a2e4116fbd3806fb1688504d94913545a
claudius <claudius.teodorescu@gmail.com>
2018-10-25 14:27:04 +0300

    Solved #3260.

--- modules/naddara-export/naddara-issues/naddara-isses.js
+++ modules/naddara-export/naddara-issues/naddara-isses.js
@@ -1,44 +1,13 @@
 AuthCheck = {};
 
 $(document).ready(function() {
-    if (localStorage.getItem('authcheck')) {
-        AuthCheck.data = JSON.parse(localStorage.getItem('authcheck'));
-    } 
-                
-    $(document).on("click", "input[name^='idStatus']", function() {
-        const $input = $(this);
-        const idStatus = ":" + $input.val();
-        const inputName = $input.attr("name").split("_");
-        const rowIndex = inputName[1];
-        const columnIndex = inputName[2];
-        const idIndex = inputName[3];
-        const idState = $input.parent().parent().children("span[data-state]").data("state");
-        const idNewState = idState.replace(/:accepted/g, "").replace(/:rejected/g, "") + idStatus;
-        $input.parent().parent().children("span[data-state]").data("state", idNewState);
-        const dataNewState = AuthCheck.table.cell(rowIndex, columnIndex).data().replace(idState, idNewState);
-        AuthCheck.table.cell(rowIndex, columnIndex).data(dataNewState);
-        var tableData = [];
-        AuthCheck.table.rows().every( function(rowIdx, tableLoop, rowLoop) {
-            var d = this.data();
-            tableData.push(this.data());
-        });        
-        
-        localStorage.setItem("authcheck", JSON.stringify(tableData));
-    });
-    
     var buttonCommon = {
-        text: 'Export CSV',
+        text: "CSV",
         exportOptions: {
             format: {
                 body: function (data, row, column, node) {
-                    if (column === 2) {
-                        var result = $(node).find("span[data-state$='accepted']").map(function() {
-                            return $(this).data("state");
-                        }).get().join(" ");
-                        result = result.replace(/:accepted/g, "");
-                        
-                        return result;
-                            
+                    if (column === 0 || column === 2) {
+                        return $(node).text();
                     } else {
                         return data;
                     }
@@ -50,7 +19,7 @@ $(document).ready(function() {
     AuthCheck.table = 
     $("#table").DataTable({
         "dom": '<"top"Bipf>rt<"bottom"lp><"clear">',
-        "data": AuthCheck.data,
+        "ajax": "naddara-issues.xql",
         "deferRender": true,
         "columnDefs": [{
             orderData: [[0, 'asc'], [1, 'asc']]
@@ -59,80 +28,18 @@ $(document).ready(function() {
         {
             "render": function (data, type, row, meta) {
                 if (data) {
-                    const ids = data.split(" ");
-                    var processedIds = [];
-                    const rowIndex = meta.row;
-                    const columnIndex = meta.col;
-                    
-                    ids.forEach(function(idState, idIndex) {
-                        const decodedIdState = decodeURIComponent(idState);
-                        const decodedId = decodedIdState.split(":");
-                        const idType = decodedId[1];
-                        const id = decodedId[3];
-                        var idStatus = "";
-                        var acceptChecked = "";
-                        var rejectChecked = "";
-                        
-                        if (decodedId.length === 5) {
-                            idStatus = decodedId[4];
-                        }
-                        if (idStatus == "accepted") {
-                            acceptChecked = ' checked="true"';
-                        }
-                        if (idStatus == "rejected") {
-                            rejectChecked = ' checked="true"';
-                        }
-                        
-                        var url = "";
-                        var idIndex = "idStatus_" + rowIndex + "_" + columnIndex + "_" + idIndex;
-                        
-                        switch (idType) {
-                            case 'viaf':
-                                url = "https://viaf.org/viaf/" + id;
-                                break;
-                            case 'dnb':
-                                url = "http://services.dnb.de/sru/dnb?operation=searchRetrieve&amp;version=1.1&amp;query=" + id;
-                            case 'wikidata':
-                                url = "https://www.wikidata.org/wiki/" + id;                                
-                            break;
-                        }
-                        var cellContent = '<div>' + idType + ': <span data-state="' + idState + '">' + decodedId[2] + ' ' + '</span><a href="' + url + '" target="_blank">' + id + '</a><br/>'
-                        + '<label><input type="radio" name="' + idIndex + '" value="accepted"' + acceptChecked + '/>Accept</label>'
-                        + '<label><input type="radio" name="' + idIndex + '" value="rejected"' + rejectChecked + '/>Reject</label>'
-                        + '</div>';
-                        
-                        processedIds.push(cellContent);
-                    }); 
-                    
-                    return processedIds.join("<br/>");
+                    return '<a href="http://kjc-sv016.kjc.uni-heidelberg.de:8080/exist/apps/tamboti/modules/search/index.html?search-field=ID&amp;value=' + data + '" target="_blank">' + data + '</a>';
                 } else {
                     return data;
                 }
             },
-            "targets": 2
+            "targets": [0, 2]
         }],
         "colReorder": true,
         "stateSave": true,
         "buttons": [
-            {
-                text: "Import CSV",
-                className: "ladda-button",
-                attr:  {
-                    "data-style": "expand-right",
-                    "id": "import-csv-button",
-                    "data-color": "blue"
-                },                    
-                action: function (e, dt, node, config) {
-                    $("#fileupload").click();
-                }
-            }
-            ,
             $.extend(true, {}, buttonCommon, {
-                extend: 'csvHtml5',
-                className: "ladda-button",
-                attr:  {
-                    "data-color": "blue"
-                },                    
+                extend: 'csvHtml5'
             })
         ]            
     })
@@ -141,21 +48,4 @@ $(document).ready(function() {
         scrollTop: $(".dataTables_wrapper").offset().top
       }, 'slow');
     });
-    
-    $("#fileupload").fileupload({
-        dataType: 'json'
-    })
-    .bind('fileuploadadd', function (e, data) {
-        AuthCheck.importCSVbutton.start();       
-        AuthCheck.table.clear().draw();
-        data.submit();
-    })
-    .bind("fileuploaddone", function (e, data) {
-        AuthCheck.table.rows.add(data.result).draw();
-        AuthCheck.importCSVbutton.stop();
-        AuthCheck.table.state.save();
-        localStorage.setItem("authcheck", JSON.stringify(data.result));
-    });
-    
-    AuthCheck.importCSVbutton = Ladda.create(document.querySelector("#import-csv-button"));
 });
--- modules/naddara-export/naddara-issues/naddara-issues.xql
+++ modules/naddara-export/naddara-issues/naddara-issues.xql
@@ -4,46 +4,28 @@ declare namespace mods = "http://www.loc.gov/mods/v3";
 declare namespace output = "http://www.w3.org/2010/xslt-xquery-serialization";
 declare namespace xlink = "http://www.w3.org/1999/xlink";
 
-declare option output:method "html";
-declare option output:media-type "text/html";
+declare option output:method "text";
+declare option output:media-type "text/javascript";
 
 let $issues := collection("/resources/commons/Abou_Naddara/")//mods:mods[mods:relatedItem/@type = 'series']
 
 
 return
-    <html xmlns="http://www.w3.org/1999/xhtml">
-        <head>
-            <title>Naddara Issues</title>
-            <meta charset="utf-8"/>
-            <link rel="stylesheet" href="naddara-issues.css"/>
-        </head>
-        <body>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Issue ID</th>
-                        <th>Series Title</th>
-                        <th>Series ID</th>
-                        <th>Date</th>
-                        <th>Issue No.</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        for $issue in $issues
-                        let $issue-id := $issue/@ID/string()
-                        let $series-id := substring-after($issue/mods:relatedItem[@type = 'series']/@xlink:href, "#")
-                        
-                        return
-                            <tr>
-                                <td><a href="http://kjc-sv016.kjc.uni-heidelberg.de:8080/exist/apps/tamboti/modules/search/index.html?search-field=ID&amp;value={$issue-id}" target="_blank">{$issue-id}</a></td>
-                                <td>{$issue/mods:titleInfo[@transliteration = 'arabic/ala-lc']/string()}</td>
-                                <td><a href="http://kjc-sv016.kjc.uni-heidelberg.de:8080/exist/apps/tamboti/modules/search/index.html?search-field=ID&amp;value={$issue-id}" target="_blank">{$series-id}</a></td>
-                                <td>{$issue/mods:originInfo/mods:dateIssued[@keyDate = 'yes']/string()}</td>
-                                <td>{$issue/mods:location/mods:url/string()}</td>
-                            </tr>                            
-                    }
-                </tbody>            
-            </table>
-        </body>
-    </html>
+    "{&quot;data&quot;:"
+    ||
+    "["
+    ||
+    string-join(
+        for $issue in $issues
+        let $issue-id := $issue/@ID/string()
+        let $series-id := substring-after($issue/mods:relatedItem[@type = 'series']/@xlink:href, "#")
+        let $array := "[&quot;" || string-join(($issue-id, normalize-space($issue/mods:titleInfo[@transliteration = 'arabic/ala-lc']/string()), $series-id, normalize-space($issue/mods:originInfo/mods:dateIssued[@keyDate = 'yes']/string()), $issue/mods:location/mods:url/string()), "&quot;,&quot;") || "&quot;]"
+            
+        return $array
+        ,
+        ","
+    )
+    ||
+    "]"
+    ||
+    "}"
